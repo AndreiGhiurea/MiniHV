17cdac39a4ebcd2aafad243401c0c28c4cd13d56
Andrei Stefan GHIUREA <andreis181@gmail.com>
2020-05-14 16:16:16 +0300

    * Allocate the output buffer on heap, stack wasn't enough

--- src/VMCALL/main.c
+++ src/VMCALL/main.c
@@ -11,7 +11,23 @@ VOID _cdecl _VmcallToHV(
 
 int main()
 {
-    CHAR outputBuffer[0x1000];
+    CHAR* outputBuffer = NULL;
+    DWORD context = 0;
+    outputBuffer = (CHAR*)malloc(0x1000);
+    if (NULL == outputBuffer)
+    {
+        printf("Malloc failed\n");
+        return 1;
+    }
+
+    _VmcallToHV(0, NULL, 0x1000, outputBuffer, &context);
+
+    CHAR processNames[255][16];
+    DWORD processPids[255];
+    DWORD nrOfProcesses = 0;
+
+    printf("Should have %d processes from HV\n", context);
+    
 
     _VmcallToHV(0, NULL, sizeof(outputBuffer), outputBuffer, NULL);
 
